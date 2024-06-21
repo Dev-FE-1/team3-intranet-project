bc2144a14a85d8a83279cce01575b32900c4aeaf
Sonseongoh <nobleser@naver.com>
2024-06-21 17:22:47 +0900

    Feat: 모달창 구현 #7

--- src/pages/admin/admin-notice/admin-notice-content.js
+++ src/pages/admin/admin-notice/admin-notice-content.js
@@ -1,14 +1,17 @@
 const modal = document.querySelector(".modal");
-const modalOpen = document.querySelector(".modal_btn");
-const modalClose = document.querySelector(".close_btn");
+const modalOpen = document.querySelector("#delete");
+const modalDelete = document.querySelector("#deleteConfirm");
+const modalDeleteCancel = document.querySelector("#deleteCancel");
 
 //열기 버튼을 눌렀을 때 모달팝업이 열림
 modalOpen.addEventListener("click", function () {
-  //'on' class 추가
+  console.log("Open button clicked."); // 버튼 클릭 시 로그 확인
   modal.classList.add("on");
+  console.log(modal.classList);
 });
 //닫기 버튼을 눌렀을 때 모달팝업이 닫힘
-modalClose.addEventListener("click", function () {
+modalDelete.addEventListener("click", function () {
   //'on' class 제거
+
   modal.classList.remove("on");
 });
