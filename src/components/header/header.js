7765791a0ac3121783fd6820851821953e24eb3c
Sumin <tnalsdldkr2@naver.com>
2024-06-26 17:42:53 +0900

    Design: header working-hours modal UI 수정 #1

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,7 @@ import absentRequest from '../../pages/admin/absent-request/absentRequest.js';
 import employeeList from '../../pages/admin/employee-list/employeeList.js';
 
 function app() {
+  initializeWorkTimeButton();
   window.addEventListener("popstate", (event) => {
     console.log("popstate");
     route();
@@ -12,12 +13,72 @@ function app() {
   route();
 }
 
-function navigatePage(event) {
-  event && event.preventDefault();
-  const anchor = event.target.closest("a");
+function initializeWorkTimeButton() {
+  const openButtons = document.querySelectorAll('.header-time');
+  const startTimeModal = document.querySelector('.start-time-modal');
+  const endTimeModal = document.querySelector('.end-time-modal');
+  const startButton = startTimeModal.querySelector('.start');
+  const endButton = endTimeModal.querySelector('.end');
+  const closeButtonStart = startTimeModal.querySelector('.close');
+  const closeButtonEnd = endTimeModal.querySelector('.close');
+  const modalBackgroundStart = startTimeModal.querySelector('.modal-background');
+  const modalBackgroundEnd = endTimeModal.querySelector('.modal-background');
 
-  // 앵커에 값이 없을 수도 있으니 방어코드 작성
+  let workStartTime;
+  let workInterval;
+
+  function toggleStartTimeModal() {
+    startTimeModal.classList.toggle('hidden');
+  }
+
+  function toggleEndTimeModal() {
+    endTimeModal.classList.toggle('hidden');
+  }
+
+  function startWork() {
+    workStartTime = Date.now();
+    workInterval = setInterval(updateWorkTime, 1000);
+    openButtons.forEach(button => button.textContent = '0시간 0분');
+    toggleStartTimeModal();
+  }
+
+  function endWork() {
+    clearInterval(workInterval);
+    openButtons.forEach(button => button.textContent = 'Working Hours');
+    toggleEndTimeModal();
+  }
+
+  function updateWorkTime() {
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - workStartTime;
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const second = Math.floor(((elapsedTime % (1000 * 60)) / 1000));
+    openButtons.forEach(button => button.textContent = `${hours}시간 ${minutes}분 ${second}초`);
+  }
+
+  openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      if (button.textContent === 'Working Hours') {
+        toggleStartTimeModal();
+      } else {
+        toggleEndTimeModal();
+      }
+    });
+  });
+
+  startButton.addEventListener('click', startWork);
+  endButton.addEventListener('click', endWork);
+  closeButtonStart.addEventListener('click', toggleStartTimeModal);
+  closeButtonEnd.addEventListener('click', toggleEndTimeModal);
+  modalBackgroundStart.addEventListener('click', toggleStartTimeModal);
+  modalBackgroundEnd.addEventListener('click', toggleEndTimeModal);
+}
+
+function navigatePage(event) {
+  const anchor = event.target.closest("a");
   if (anchor && anchor.href) {
+    event.preventDefault();
     const path = anchor.getAttribute("href");
     history.pushState(null, null, path);
     route();
@@ -25,7 +86,6 @@ function navigatePage(event) {
 }
 
 function route() {
-  // const content = document.querySelector("#app");
   const path = location.pathname;
 
   switch (path) {
