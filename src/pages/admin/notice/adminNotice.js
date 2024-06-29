4c70b0f8df8b31ae2dd9686d0a5b77905c30ec07
Sonseongoh <nobleser@naver.com>
2024-06-30 03:27:32 +0900

    Style: adminNotice,userNotice관련 파일들 class명앞에 각 파일명 추가 #3

--- src/pages/admin/notice/adminNotice.js
+++ src/pages/admin/notice/adminNotice.js
@@ -5,28 +5,28 @@ import "../../admin/admin-notice/adminNoticeContent.css";
 
 export default function adminNotice(container) {
   const content = document.querySelector(container);
-  content.innerHTML = `<div class="container">
-      <div class="content-wrapper">
-        <div class="content">
-          <div class="board" data-path="/notice/noticeContent">
-            <div class="content-image">이미지존</div>
-            <div class="content-title">이서미님의 퇴사</div>
+  content.innerHTML = `<div class="adminNotice-container">
+      <div class="adminNotice-content-wrapper">
+        <div class="adminNotice-content">
+          <div class="adminNotice-board" data-path="/notice/noticeContent">
+            <div class="adminNotice-content-image">이미지존</div>
+            <div class="adminNotice-content-title">이서미님의 퇴사</div>
           </div>
-          <div class="board" data-path="/notice/noticeContent">
-            <div class="content-image">이미지존</div>
-            <div class="content-title">이서미님의 퇴사</div>
+          <div class="adminNotice-board" data-path="/notice/noticeContent">
+            <div class="adminNotice-content-image">이미지존</div>
+            <div class="adminNotice-content-title">이서미님의 퇴사</div>
           </div>
-          <div class="board" data-path="/notice/noticeContent">
-            <div class="content-image">이미지존</div>
-            <div class="content-title">이서미님의 퇴사</div>
+          <div class="adminNotice-board" data-path="/notice/noticeContent">
+            <div class="adminNotice-content-image">이미지존</div>
+            <div class="adminNotice-content-title">이서미님의 퇴사</div>
           </div>
-          <button class="add-button" data-path="/notice/noticeCreate">+</button>
+          <button class="adminNotice-add-button" data-path="/notice/noticeCreate">+</button>
         </div>
       </div>
     </div>`;
 
-  const addButton = document.querySelector(".add-button");
-  const boards = document.querySelectorAll(".board");
+  const addButton = document.querySelector(".adminNotice-add-button");
+  const boards = document.querySelectorAll(".adminNotice-board");
 
   boards.forEach((board) => {
     board.addEventListener("click", (event) => {
@@ -51,22 +51,22 @@ export default function adminNotice(container) {
 
 export function adminNoticeCreate(container) {
   const content = document.querySelector(container);
-  content.innerHTML = `<div class="container">
-      <div class="content-wrapper">
-        <div class="content">
-          <div class="title-box">
+  content.innerHTML = `<div class="adminNoticeCreate-container">
+      <div class="adminNoticeCreate-content-wrapper">
+        <div class="adminNoticeCreate-content">
+          <div class="adminNoticeCreate-title-box">
             제목
             <input type="text" placeholder="제목을 입력하세요" />
           </div>
-          <div class="description">
+          <div class="adminNoticeCreate-description">
             내용
             <textarea type="text" placeholder="내용을 입력하세요"></textarea>
           </div>
-          <div class="image-upload">
+          <div class="adminNoticeCreate-image-upload">
             첨부 사진
-            <div class="image-upload-box"></div>
+            <div class="adminNoticeCreate-image-upload-box"></div>
           </div>
-          <div class="button-zone">
+          <div class="adminNoticeCreate-button-zone">
             <button id="cancel">취소</button>
             <button id="submit">등록</button>
           </div>
@@ -85,27 +85,27 @@ export function adminNoticeCreate(container) {
 export function adminNoticeContent(container) {
   const content = document.querySelector(container);
   content.innerHTML = `
-   <div class="container">
-      <div class="content-wrapper">
-        <div class="content">
-          <div class="title-box">
+   <div class="adminNoticeContent-container">
+      <div class="adminNoticeContent-content-wrapper">
+        <div class="adminNoticeContent-content">
+          <div class="adminNoticeContent-title-box">
             <div id="title">이서미님의 퇴사</div>
             <div id="writer">이서미</div>
           </div>
-          <hr class="divider" />
-          <div class="img-box">이미지</div>
-          <div class="description-box">공지내용문구</div>
-          <hr class="divider2" />
-          <div class="button-zone">
+          <hr class="adminNoticeContent-divider" />
+          <div class="adminNoticeContent-img-box">이미지</div>
+          <div class="adminNoticeContent-description-box">공지내용문구</div>
+          <hr class="adminNoticeContent-divider2" />
+          <div class="adminNoticeContent-button-zone">
             <button id="delete">삭제</button>
             <button id="submit">수정</button>
           </div>
 
           <!-- 모달창 -->
-          <div id="modal" class="modal">
-            <div class="modal-content">
+          <div id="modal" class="adminNoticeContent-modal">
+            <div class="adminNoticeContent-modal-content">
               <h4>정말 삭제 하시겠습니까?</h4>
-              <div class="button-zone">
+              <div class="adminNoticeContent-button-zone">
                 <button id="deleteCancel">취소</button>
                 <button id="deleteConfirm">확인</button>
               </div>
